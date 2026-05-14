1bfa4451707dab2885dae63458a127bfa9416c80
Nadia <nvbeznosova@gmail.com>
2026-05-14 17:38:20 +0300

    fix: add explicit waits, remove old driver fixture from registration

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -1,8 +1,10 @@
-import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from locators.constructor_page_locators import ConstructorPageLocators
 
 def test_constructor_tabs(driver):
     driver.get("https://stellarburgers.education-services.ru/")
-    driver.find_element(*ConstructorPageLocators.BUNS_TAB).click()
-    driver.find_element(*ConstructorPageLocators.SAUCES_TAB).click()
-    driver.find_element(*ConstructorPageLocators.FILLINGS_TAB).click()
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.element_to_be_clickable(ConstructorPageLocators.BUNS_TAB)).click()
+    wait.until(EC.element_to_be_clickable(ConstructorPageLocators.SAUCES_TAB)).click()
+    wait.until(EC.element_to_be_clickable(ConstructorPageLocators.FILLINGS_TAB)).click() 
--- tests/test_personal_account.py
+++ tests/test_personal_account.py
@@ -1,4 +1,6 @@
 import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from locators.personal_account_locators import PersonalAccountLocators
 
 TEST_EMAIL = os.environ.get("TEST_EMAIL")
@@ -6,16 +8,18 @@ TEST_PASSWORD = os.environ.get("TEST_PASSWORD")
 
 def test_redirect_to_personal_account(driver):
     driver.get("https://stellarburgers.education-services.ru/login")
-    driver.find_element(*PersonalAccountLocators.EMAIL_INPUT).send_keys(TEST_EMAIL)
-    driver.find_element(*PersonalAccountLocators.PASSWORD_INPUT).send_keys(TEST_PASSWORD)
-    driver.find_element(*PersonalAccountLocators.LOGIN_BUTTON).click()
-    driver.find_element(*PersonalAccountLocators.PERSONAL_ACCOUNT_BUTTON).click()
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.EMAIL_INPUT)).send_keys(TEST_EMAIL)
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.PASSWORD_INPUT)).send_keys(TEST_PASSWORD)
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.LOGIN_BUTTON)).click()
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.PERSONAL_ACCOUNT_BUTTON)).click()
     assert "account" in driver.current_url
 
 def test_logout_from_personal_account(driver):
     driver.get("https://stellarburgers.education-services.ru/login")
-    driver.find_element(*PersonalAccountLocators.EMAIL_INPUT).send_keys(TEST_EMAIL)
-    driver.find_element(*PersonalAccountLocators.PASSWORD_INPUT).send_keys(TEST_PASSWORD)
-    driver.find_element(*PersonalAccountLocators.LOGIN_BUTTON).click()
-    driver.find_element(*PersonalAccountLocators.LOGOUT_BUTTON).click()
-    assert "login" in driver.current_url
+    wait = WebDriverWait(driver, 10)
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.EMAIL_INPUT)).send_keys(TEST_EMAIL)
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.PASSWORD_INPUT)).send_keys(TEST_PASSWORD)
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.LOGIN_BUTTON)).click()
+    wait.until(EC.element_to_be_clickable(PersonalAccountLocators.LOGOUT_BUTTON)).click()
+    assert "login" in driver.current_url 
